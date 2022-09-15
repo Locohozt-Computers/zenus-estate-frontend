822432d41920d59dd916dc7886c19bfe6b05d00e
giftdavid101 <giftdavid101@gmail.com>
2022-09-15 16:19:11 +0100

    my-account

--- src/pages/MyAccountPage.tsx
+++ src/pages/MyAccountPage.tsx
@@ -4,6 +4,9 @@ import { getUserProfile } from "pages/request";
 import { Loader } from "components/atoms/Loader";
 import { FormikProvider, useFormik, useFormikContext } from "formik";
 import styled from "styled-components/macro";
+import { Input } from "components/atoms";
+import { Typography } from "components";
+import house from "../assets/images/img.png";
 
 const InputStyling = styled.input<{ error?: boolean }>``;
 
@@ -37,6 +40,38 @@ const EditableInput = ({
     </div>
   );
 };
+const MyAccountStyle = styled.div`
+  .editableInput {
+    //background: red !important;
+    border-radius: 0 !important;
+    border-bottom: 1px solid #ededed !important;
+    background: none !important;
+    padding: 0.5rem 0 !important;
+  }
+  .input-container {
+    margin-bottom: 35px;
+  }
+  fieldset {
+    border: none;
+  }
+  legend {
+    margin-bottom: 37px;
+  }
+`;
+
+const MyAccountHeader = styled.div`
+  background: darkcyan;
+  height: 192px;
+  margin-bottom: 35px;
+  border-radius: 10px;
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  max-height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+`;
 
 const MyAccountPage = () => {
   const { isLoading, data } = useQuery(["getUserProfile"], getUserProfile);
@@ -61,16 +96,109 @@ const MyAccountPage = () => {
   }, [data]);
 
   return (
-    <div style={{ position: "relative" }}>
+    <MyAccountStyle style={{ position: "relative" }}>
       <Loader open={isLoading} absolute />
+      <MyAccountHeader style={{ backgroundImage: `url(${house})` }}>
+        <Typography
+          style={{
+            width: "110px",
+            height: "110px",
+            borderRadius: "50%",
+            background: "var(--blue)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            border: "2.3px solid white",
+            position: "relative",
+            top: "132px",
+          }}
+          size={34}
+          textColor="white"
+        >
+          DM
+        </Typography>
+      </MyAccountHeader>
       <FormikProvider value={formik}>
+        <div>
+          <Input
+            readOnly
+            label="Estate Owner"
+            name="amount"
+            className="editableInput"
+            placeholder="Daniel Mbazu"
+          />
+        </div>
+        <Input
+          readOnly
+          label="Address"
+          name="address"
+          className="editableInput"
+          placeholder="12 Okue Street, Okota, Mainland."
+        />{" "}
+        <Input
+          readOnly
+          label="Email"
+          name="amount"
+          className="editableInput"
+          placeholder="danielmbazu9@gmail.com"
+          type="email"
+        />{" "}
+        <Input
+          readOnly
+          label="Account Details"
+          name="account"
+          className="editableInput"
+          placeholder="Daniel Mbazu"
+        />{" "}
+        <Input
+          readOnly
+          label="Phone Number"
+          name="phoneNumber"
+          className="editableInput"
+          placeholder="Daniel Mbazu"
+        />{" "}
+        <Input
+          readOnly
+          label="Password"
+          name="amount"
+          className="editableInput"
+          type="password"
+        />
+        <fieldset>
+          <legend>
+            <Typography weight={600} size={20} textColor="blue">
+              LandLord&apos;s Details{" "}
+            </Typography>
+          </legend>
+          <Input
+            readOnly
+            label="Name"
+            name="name"
+            className="editableInput"
+            placeholder="Daniel Mbazu"
+          />{" "}
+          <Input
+            readOnly
+            label="Phone Number"
+            name="phoneNumber"
+            className="editableInput"
+            placeholder="Daniel Mbazu"
+          />{" "}
+          <Input
+            readOnly
+            label="Email Adress"
+            name="phoneNumber"
+            className="editableInput"
+            placeholder="Daniel Mbazu"
+          />{" "}
+        </fieldset>
         <EditableInput label="Estate Owner" name="estateOwner" />
         <EditableInput label="Address" name="address" />
         <EditableInput label="Account Number" name="accountNumber" />
         <EditableInput label="Phone Number" name="phoneNumber" />
         <EditableInput label="Email" name="email" />
       </FormikProvider>
-    </div>
+    </MyAccountStyle>
   );
 };
 
