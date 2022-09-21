7fefd80d59f8cf42f29f8ffe7d4bf1f14cd716ce
kristofajosh <christopherjoshua18@live.com>
2022-09-21 12:39:18 +0100

    fix: update profile type

--- src/api/types.ts
+++ src/api/types.ts
@@ -81,6 +81,18 @@ export class GetLandlordProfile {
   };
 }
 
+interface BankAccountResI {
+  id: number;
+  user_id: number;
+  bank_code: number;
+  account_number: string;
+  account_name?: string;
+  status: number;
+  branch_id: number;
+  created_at: string;
+  updated_at: string;
+}
+
 export class GetProfile {
   static Route = "/profile";
 
@@ -107,6 +119,7 @@ export class GetProfile {
       created_at: string;
       updated_at: string;
       walletBalance: number;
+      bank?: BankAccountResI;
     };
   };
 }
@@ -520,18 +533,6 @@ export class GetWalletTransactions {
   };
 }
 
-interface BankAccountResI {
-  id: number;
-  user_id: number;
-  bank_code: number;
-  account_number: string;
-  account_name?: string;
-  status: number;
-  branch_id: number;
-  created_at: string;
-  updated_at: string;
-}
-
 export class PostFundWallet {
   static Route = "/fund-wallet";
 
@@ -615,9 +616,9 @@ export class GetAllNotifications {
     status: string;
     message: string;
     data: Array<{
-      id: "5a425fe1-a1b5-48ab-bf7c-ff3a8fe0ae64";
-      message: "You  successfully funded your wallet with =N= 69000 ";
-      created_at: "2022-09-18T10:44:59.000000Z";
+      id: string;
+      message: string;
+      created_at: string;
       read_at: null;
     }>;
   };
